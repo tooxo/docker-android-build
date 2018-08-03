90eaee278c72420640554bcae0c2787658274de9
Thomas Kruse <thomas.kruse@reucon.com>
2018-08-04 01:19:30 +0200

    emulator support

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,12 +14,19 @@ mkdir -p $HOME/.android/avd/Docker.avd
 ) >> $HOME/.android/avd/Docker.avd/config.ini
 
 export DISPLAY=:0
-emulator -avd Docker -no-skin -no-audio -no-window &
+emulator -avd Docker -no-skin -no-audio -no-window -no-boot-anim &
 
 #wait for boot
+echo "Waiting for device.."
 adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
 
+#disable animations
+adb shell settings put global window_animation_scale 0.0
+adb shell settings put global transition_animation_scale 0.0
+adb shell settings put global animator_duration_scale 0.0
+
 #unlock screen
+echo "Unlocking screen..."
 adb shell wm dismiss-keyguard
 sleep 5
 
